564769d20397108f66120ef12ad2ad5b4865c26d
Oskar Ramirez <oskarramirez43@gmail.com>
2018-03-16 14:37:52 -0400

    variables ch

---
+++ variables/age.rb
@@ -0,0 +1,11 @@
+#2
+puts "How old are you? "
+age = gets.chomp
+
+puts "In 10 years you will be: #{age.to_i+10}"
+puts "In 20 years you will be: #{age.to_i+20}"
+puts "In 30 years you will be: #{age.to_i+30}"
+puts "In 40 years you will be: #{age.to_i+40}"
+
+#4
+Outputs 3 and undefined as the second x was created in the local scope.
---
+++ variables/name.rb
@@ -0,0 +1,19 @@
+#1
+puts "Enter your name: "
+name = gets.chomp
+
+puts "Hello #{name}!!!"
+#3
+i=1
+while i<=10
+  puts name
+  i+=1
+end
+
+#4
+puts "Kindly enter your first name"
+first_name = gets.chomp
+puts "Kindly enter last name"
+last_name = gets.chomp
+
+puts "#{first_name} #{last_name}"
